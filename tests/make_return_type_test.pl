4ad4e4169287b513180ae84b84e605b898efcba8
Jan Marthedal Rasmussen <jmr@kanooth.com>
2011-10-05 12:46:41 +0200

    New type check idea

--- tests/make_return_type_test.pl
+++ tests/make_return_type_test.pl
@@ -30,6 +30,11 @@ EOF
 
 my %assigns = ();
 
+sub is_long_long {
+    $type = shift;
+    return $type eq "long long" || $type eq "unsigned long long";
+}
+
 while (<>) {
     if (/^\s*#/) {
         # ignore
@@ -37,22 +42,31 @@ while (<>) {
         @ops = split(/\s*,\s*/, $1);
     } elsif (/^\s*([a-zA-Z_ ]*?)\s*,\s*([a-zA-Z_ ]*?)\s*$/) {
         foreach (@ops) {
+            $long_long_guard = is_long_long($1) || is_long_long($2);
+            print "#ifdef KANOOTH_HAS_LONG_LONG\n" if $long_long_guard;
             print "  static_assert<kanooth::numbers::${_}_enabled<$1, $2>::enabled>();\n";
+            print "#endif\n" if $long_long_guard;
             if ($_ == "set") {
                 push(@{$assigns{$2}}, $1);
             }
-        }        
+        }
     } elsif (/^\s*([a-zA-Z_ ]*?)\s*<-\s*([a-zA-Z_ ]*?)\s*,\s*([a-zA-Z_ ]*?)\s*$/) {
         foreach (@ops) {
             $op = $_;
+            $long_long_guard = is_long_long($1) || is_long_long($2) || is_long_long($3);
+            print "#ifdef KANOOTH_HAS_LONG_LONG\n" if $long_long_guard;
             print "  static_assert_type<$1, typename kanooth::numbers::${op}_result<$2, $3>::type>();\n";
             if (defined $assigns{$1}) {
                 foreach (@{$assigns{$1}}) {
-                    print "  static_assert<kanooth::numbers::${op}_enabled<${_}, $2, $3>::enabled>();\n";                
+                    $long_long_guard2 = is_long_long($_) && !$long_long_guard;
+                    print "#ifdef KANOOTH_HAS_LONG_LONG\n" if $long_long_guard2;
+                    print "  static_assert<kanooth::numbers::${op}_enabled<${_}, $2, $3>::enabled>();\n";
+                    print "#endif\n" if $long_long_guard2;
                 }
             } else {
                 print "  static_assert<kanooth::numbers::${op}_enabled<$1, $2, $3>::enabled>();\n";
             }
+            print "#endif\n" if $long_long_guard;
         }
     }
 }
